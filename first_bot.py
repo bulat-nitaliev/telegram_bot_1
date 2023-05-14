ae74ff899269f9f1bf2e8bb7124d7066597d10c5
rauf.akchurin <abdurauf.dev@gmail.com>
2023-05-14 12:06:30 +0500

    1

---
+++ first_bot.py
@@ -0,0 +1,22 @@
+# Import the necessary modules
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+# Create a bot instance
+bot = Bot(token='YOUR_API_TOKEN_HERE')
+
+# Create a dispatcher instance
+dp = Dispatcher(bot)
+
+# Define a message handler that echoes back the received message
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+# Start the bot using the executor
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
