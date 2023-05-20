22ac764822f1237a98e1cd2ac1cecb46d9c17133
Rifat <nafikovrifat@gmail.com>
2023-05-20 20:13:40 +0600

    add function for getting chat id

--- first_bot.py
+++ first_bot.py
@@ -18,6 +18,12 @@ bot = Bot(token=TOKEN)
 # Create a dispatcher instance
 dp = Dispatcher(bot)
 
+# Command handler for /chatid
+@dp.message_handler(commands=['chatid'])
+async def chatid(message: types.Message):
+    chat_id = message.chat.id
+    await message.answer(f"Ваш Chat ID: {chat_id}")
+
 # Define a message handler that echoes back messages
 @dp.message_handler()
 async def echo(message: types.Message):
