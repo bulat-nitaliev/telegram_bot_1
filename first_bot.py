9f03e90028680aabdd23c630ab52eeedd581a032
rauf.akchurin <abdurauf.dev@gmail.com>
2023-05-22 12:10:20 +0500

    test ssh

--- first_bot.py
+++ first_bot.py
@@ -25,6 +25,7 @@ async def echo(message: types.Message):
     await message.answer(message.text)
 
 
+
 # Start the bot using the executor
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
