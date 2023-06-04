43382b42ad02bfd1c02ac42fe146e3fea84e6551
Rinat Shamsutdinov <rinat20@bk.ru>
2023-06-05 01:42:22 +0300

    1111

--- new_member.py
+++ new_member.py
@@ -5,7 +5,8 @@ from aiogram.utils import executor
 from config import TOKEN, GROUP_ID, BOT_NAME, DEV_ID
 from static.data import greeting_text, id_passed_text, instructions
 from stepik import stepik_data, get_stepik_token
-from models import Student, session
+from models import Student, Result, session
+from datetime import date
 
 
 logging.basicConfig(level=logging.INFO)
@@ -41,23 +42,37 @@ async def welcome(message: types.Message):
 async def private_msgs(message: types.Message):
     chat_id = message.chat.id
     await bot.send_message(chat_id=chat_id, text=instructions)
-    try:
-        stepik_id = int(message.text.split("/")[-1])
-        user_data_url = f"https://stepik.org:443/api/users/{stepik_id}"
-        user_data = stepik_data(user_data_url, stepik_token)
-        name = user_data['users'][0]['full_name']
-        tg_id = message.from_user.id
-        tg_full_name = message.from_user.full_name
-        new_obj = Student(
+    
+    stepik_id = int(message.text.split("/")[-1])
+    user_data_url = f"https://stepik.org:443/api/users/{stepik_id}"
+    user_data = stepik_data(user_data_url, stepik_token)
+    name = user_data['users'][0]['full_name']
+    tg_id = message.from_user.id
+    tg_full_name = message.from_user.full_name
+    new_obj = Student(
             id = stepik_id,
             name = name,
             tg_id = tg_id,
             tg_full_name = tg_full_name,
             tg_username = message.from_user.username
         )
-        session.merge(new_obj)
-        session.commit()
-        await bot.restrict_chat_member(
+    
+    result_data_url =f'https://stepik.org:443/api/course-grades?course=58852&user={stepik_id}'
+    result_data = stepik_data(result_data_url, stepik_token)
+    course_id = result_data['course-grades'][0]['course']
+    score = result_data['course-grades'][0]['score']
+    update_date = date.today()
+    
+    new_objects = Result(        
+        student_id = stepik_id,      
+        course_id = course_id,
+        score = score,
+        update_date = update_date
+    )
+    session.merge(new_obj)
+    session.add(new_objects)
+    session.commit()
+    await bot.restrict_chat_member(
             chat_id=GROUP_ID, 
             user_id=tg_id, 
             permissions=types.ChatPermissions(
@@ -65,12 +80,8 @@ async def private_msgs(message: types.Message):
                 can_send_media_messages=True
             )
         )
-        invite_link = await bot.export_chat_invite_link(GROUP_ID)
-        await message.answer(text=id_passed_text % invite_link)
-
-    except Exception as e:
-        await bot.send_message(chat_id=DEV_ID, text=f'{e}\n from user {tg_full_name}')
-
+    invite_link = await bot.export_chat_invite_link(GROUP_ID)
+    await message.answer(text=id_passed_text % invite_link)  
 
 if __name__ == '__main__':
     stepik_token = get_stepik_token()
